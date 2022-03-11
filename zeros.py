30ab01afb3a70e8800fbec320de44ca2907ab651
TinierClover041 <100947807+TinierClover041@users.noreply.github.com>
2022-03-11 17:55:54 -0300

    Update zeros.py

--- zeros.py
+++ zeros.py
@@ -1,18 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Crie um programa que peça um número no terminal e conte a quantidade de 
-zeros à esquerda do mesmo.
-
-* while: 1
-* str: 1
-"""
-
-while number == "0":
+number = input("n: ")
+n = 0
+while number.startswith("0"):
     n += 1
-    n = 0
-    number = input("n: ")
-    while number[0] == "0":
-        number = int(number)
-        number = number[1:]
-        while number.startswith("0"):
-            print(f"numero de zeros = {n}")
+    number = number[1:]
+        #number = int(number)
+print(f"número de zeros = {n}")
+
+#while number == "0":
+#while number[0] == "0":
